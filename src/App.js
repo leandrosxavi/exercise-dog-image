4b18f3b22fc64411ce95d70573ec4ccfbf664a54
Leandro Xavier <leandrosxavi@gmail.com>
2021-08-13 19:48:47 -0300

    Adiciona primeira parte do exercicio dos dogs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      dogObj: undefined,
+      dogArray: [],
+    };
+
+    // this.fetchDog = this.fetchDog.bind(this);
+    this.saveDog = this.saveDog.bind(this);
+    this.renderDogElement = this.renderDogElement.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  async fetchDog() {
+    this.setState({ loading: true }, async () => {
+      const dogRequest = await fetch('https://dog.ceo/api/breeds/image/random');
+      const dogData = await dogRequest.json();
+      this.setState({
+        dogObj: dogData,
+        loading: false,
+      });
+    });
+  }
+
+  saveDog() {
+    this.setState(({ dogObj, dogArray }) => ({
+      dogArray: [...dogArray, dogObj],
+    }));
+
+    this.fetchDog();
+  }
+
+  renderDogElement() {
+    const { dogObj } = this.state;
+    return (
+      <div>
+        <img src={ dogObj.message } alt="Dog" />
+        <input
+          type="button"
+          value="Gerar doguinho"
+          onClick={ this.saveDog }
+        />
+      </div>
+    );
+  }
+
+  render() {
+    const { loading, dogArray } = this.state;
+
+    return (
+      <div className="App">
+        <span>
+          { dogArray.map((dog, id) => <img key={ id } src={ dog.message } alt="Dog" />) }
+        </span>
+
+        { loading ? <span>Loading...</span> : this.renderDogElement() }
+      </div>
+    );
+  }
 }
 
 export default App;
