511519aab7855f62b0c875af6e67b227c3ac3b4f
Leandro Xavier <leandrosxavi@gmail.com>
2021-08-16 18:35:32 -0300

    Esqueci de salvar o Exemplo2.js

---
+++ src/Exemplo2.js
@@ -0,0 +1,36 @@
+// import React from "react";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: ""
+//     };
+//     this.fetchDog = this.fetchDog.bind(this);
+//   }
+
+//   componentDidMount() {
+//     this.fetchDog();
+//   }
+
+//   fetchDog() {
+//     fetch("https://dog.ceo/api/breeds/image/random")
+//       .then(res => res.json())
+//       .then(result => this.setState({ data: result }));
+//   }
+
+//   render() {
+//     if (this.state.data === "") return "loading...";
+//     return (
+//       <div>
+//         <p>Doguinhos</p>
+//         <button onClick={this.fetchDog}>Novo doguinho!</button>
+//         <div>
+//           <img src={this.state.data.message} alt="Random dog" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
