853f6fef0db1d4910e6d86cd88ad6a5819238a95
Leandro Xavier <leandrosxavi@gmail.com>
2021-08-16 18:34:57 -0300

    Adiciona exercicio dos dogs satisfatoriamente concluido.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,125 @@
-import React from "react";
+import React from 'react';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      data: ""
+      dogObj: {
+        message: '',
+      },
+      loading: true,
+      dogName: '',
+      dogArray: [],
     };
+
     this.fetchDog = this.fetchDog.bind(this);
+    this.handleEvent = this.handleEvent.bind(this);
+    this.saveDog = this.saveDog.bind(this);
+    this.fetchLastDog = this.fetchLastDog.bind(this);
   }
 
   componentDidMount() {
-    this.fetchDog();
+    if (localStorage.length > 0) {
+      this.fetchLastDog();
+    } else {
+      this.fetchDog();
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.dogObj.message.includes('terrier')) {
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { dogObj, dogArray } = this.state;
+    if (prevState.dogObj !== dogObj) {
+      localStorage.setItem('Url do doguinho', dogObj.message);
+      localStorage.setItem('Array de doguinhos', dogArray);
+      alert(dogObj.message.split('/')[4]);
+    }
+  }
+
+  handleEvent({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  fetchLastDog() {
+    const lastDog = localStorage.getItem('Url do doguinho');
+    this.setState({
+      dogObj: { message: lastDog },
+      loading: false,
+    });
   }
 
   fetchDog() {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then(res => res.json())
-      .then(result => this.setState({ data: result }));
+    this.setState({ loading: true });
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((result) => this.setState({
+        dogObj: result,
+        loading: false,
+      }));
+  }
+
+  saveDog() {
+    const { dogName } = this.state;
+    if (dogName !== '') {
+      localStorage.setItem('Nome do Doguinho', dogName);
+      this.setState((prevState) => ({
+        dogArray: [...prevState.dogArray, dogName],
+        dogName: '',
+      }));
+    }
+    this.fetchDog();
+  }
+
+  clearStorage() {
+    localStorage.clear();
+  }
+
+  renderLoading() {
+    return <span>Carregando novo dog...</span>;
+  }
+
+  renderDog() {
+    const { dogObj } = this.state;
+    const dogBreed = dogObj.message.split('/')[4];
+    return <img src={ dogObj.message } alt={ dogBreed } height="300" />;
   }
 
   render() {
-    if (this.state.data === "") return "loading...";
+    const { loading, dogName, dogArray } = this.state;
+
     return (
       <div>
-        <p>Doguinhos</p>
-        <button onClick={this.fetchDog}>Novo doguinho!</button>
-        <div>
-          <img src={this.state.data.message} alt="Random dog" />
-        </div>
+        <h1>Doguinhos</h1>
+        { !loading ? this.renderDog() : this.renderLoading() }
+        <input
+          type="text"
+          name="dogName"
+          value={ dogName }
+          placeholder="Dê nome ao doguinho"
+          onChange={ this.handleEvent }
+          required
+        />
+        <input
+          type="button"
+          value="Novo Dog"
+          onClick={ this.saveDog }
+        />
+        <input
+          type="button"
+          value="Limpar LocalStorage"
+          onClick={ this.clearStorage }
+        />
+        { dogArray.map((dog, i) => <div key={ i }><span>{ dog }</span></div>) }
       </div>
     );
   }
