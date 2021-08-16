7e6fc719c7e5239254c663b51a676d0469addb30
Leandro Xavier <leandrosxavi@gmail.com>
2021-08-16 20:27:01 -0300

    Testando Bulma

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -85,23 +86,24 @@ class App extends React.Component {
   }
 
   renderLoading() {
-    return <span>Carregando novo dog...</span>;
+    return <span className="content">Carregando novo dog...</span>;
   }
 
   renderDog() {
     const { dogObj } = this.state;
     const dogBreed = dogObj.message.split('/')[4];
-    return <img src={ dogObj.message } alt={ dogBreed } height="300" />;
+    return <img className="block" src={ dogObj.message } alt={ dogBreed } height="300" />;
   }
 
   render() {
     const { loading, dogName, dogArray } = this.state;
 
     return (
-      <div>
-        <h1>Doguinhos</h1>
+      <div className="main content">
+        <h1 className="block is-centered">Doguinhos</h1>
         { !loading ? this.renderDog() : this.renderLoading() }
         <input
+          className="input block"
           type="text"
           name="dogName"
           value={ dogName }
@@ -109,16 +111,20 @@ class App extends React.Component {
           onChange={ this.handleEvent }
           required
         />
-        <input
-          type="button"
-          value="Novo Dog"
-          onClick={ this.saveDog }
-        />
-        <input
-          type="button"
-          value="Limpar LocalStorage"
-          onClick={ this.clearStorage }
-        />
+        <div className="buttons is-centered box">
+          <input
+            className="button is-primary is-link is-medium"
+            type="button"
+            value="Novo Dog"
+            onClick={ this.saveDog }
+          />
+          <input
+            className="button is-primary is-link is-medium"
+            type="button"
+            value="Limpar LocalStorage"
+            onClick={ this.clearStorage }
+          />
+        </div>
         { dogArray.map((dog, i) => <div key={ i }><span>{ dog }</span></div>) }
       </div>
     );
